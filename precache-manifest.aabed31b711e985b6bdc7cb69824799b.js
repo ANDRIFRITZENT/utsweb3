43a06c11943eb4682abcc777fba0939864b06838
ANDRIFRITZENT <andryfritzent05@gmail.com>
2019-04-11 09:52:58 +0700

    Updates

---
+++ precache-manifest.aabed31b711e985b6bdc7cb69824799b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ANDRIFRITZENT/utsweb3/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "edc8e0d297a1c628a788",
+    "url": "/ANDRIFRITZENT/utsweb3/static/js/runtime~main.99882d2f.js"
+  },
+  {
+    "revision": "ad0f0bcc808cf5bf88fe",
+    "url": "/ANDRIFRITZENT/utsweb3/static/js/main.057112cc.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ANDRIFRITZENT/utsweb3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ad0f0bcc808cf5bf88fe",
+    "url": "/ANDRIFRITZENT/utsweb3/static/css/main.5420bb9d.chunk.css"
+  },
+  {
+    "revision": "540035e33d4df4893029bf312cd5bf7b",
+    "url": "/ANDRIFRITZENT/utsweb3/index.html"
+  }
+];
